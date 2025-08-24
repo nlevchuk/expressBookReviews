7822aadd21a6d099ddf1bc8763e41142f3fb36e3
Nikita Levchuk <code@levch.uk>
2025-08-24 17:11:34 +0200

    migrate from CJS to ESM
    fix formatting

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,26 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import session from 'express-session';
+import { authenticated as customer_routes } from './router/auth_users.js';
+import { general as genl_routes }  from './router/general.js';
+
+const PORT =5000;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use('/customer', session({
+  secret: 'fingerprint_customer',
+  resave: true,
+  saveUninitialized: true,
+}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", (req, res, next) => {
+  //Write the authenication mechanism here
 });
- 
-const PORT =5000;
 
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+app.use('/customer', customer_routes);
+app.use('/', genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log('Server is running'));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,42 @@
-const express = require('express');
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
+import express from 'express';
+import books from './booksdb.js';
+import { isValid, users } from './auth_users.js';
+
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
+public_users.post('/register', (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-module.exports.general = public_users;
+export { public_users as general }
